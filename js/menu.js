681c119148f1295769392a7457bd59714c69ae47
Nick Bolles <nickbolles@live.com>
2014-06-23 14:44:56 -0500

    Changed Margins to be compatable with a % menusize

--- js/menu.js
+++ js/menu.js
@@ -48,7 +48,7 @@ $(function(){
                 "height": "100%",
                 "display": "block !important",
                 "float": "left",
-                "margin-left": -menuSize,
+                "margin-right": menuSize,
                 "margin-top": function(){
                     if (!menuOverHeader){
                         return $("#header").height();
@@ -198,7 +198,7 @@ $(function(){
                     */
                    menuStatus = true;
                     $(menu).animate({
-                          marginLeft: "+="+(menuSize)+"px",
+                          marginRight: "-="+(menuSize),
                     }, 300);
                     shadeStatus(menuStatus);
                     return false;
@@ -209,7 +209,7 @@ $(function(){
                         marginLeft: "0px",
                     }, 300);*/
                     $(menu).animate({
-                          marginLeft: "-=" + (menuSize) + "px",
+                          marginRight: "+=" + (menuSize),
                     }, 300);
                     shadeStatus(menuStatus);
                     return false;
